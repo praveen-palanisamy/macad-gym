06d4d7ed7d4524e1128e36a236266489bb7137b3
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-12-11 10:59:54 -0500

    Added test for rllib compat check.

---
+++ tests/test_rllib_compat.py
@@ -0,0 +1,30 @@
+from gym.spaces import Box
+from env.carla.multi_env import MultiCarlaEnv
+from ray.rllib.evaluation.policy_evaluator import PolicyEvaluator
+from ray.rllib.agents.pg.pg_policy_graph import PGPolicyGraph
+
+# TODO: Add test specific MultiEnv configs
+# TODO: Tests should run with render=False
+BATCH_COUNT = 2
+EPISODE_HORIZON = 10
+
+
+def test_rllib_policy_eval():
+    evaluator = PolicyEvaluator(
+        env_creator=lambda _: MultiCarlaEnv(),
+        policy_graph={
+            "def_policy": (PGPolicyGraph, Box(0.0, 255.0, shape=(84, 84, 3)),
+                           Box(-1.0, 1.0, shape=(2, )), {
+                               "gamma": 0.99
+                           })
+        },
+        policy_mapping_fn=lambda agent_id: "def_policy",
+        batch_steps=BATCH_COUNT,
+        episode_horizon=EPISODE_HORIZON)
+
+    samples, count = evaluator.sample_with_count()
+    assert count == BATCH_COUNT
+
+
+if __name__ == "__main__":
+    test_rllib_policy_eval()
