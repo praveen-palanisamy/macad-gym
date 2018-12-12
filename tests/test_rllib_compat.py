d24ba713135b614c342a5ce453416fba917333a8
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-12-11 22:31:21 -0500

    Updated test to use headless mode

--- tests/test_rllib_compat.py
+++ tests/test_rllib_compat.py
@@ -1,5 +1,6 @@
 from gym.spaces import Box
 from env.carla.multi_env import MultiCarlaEnv
+from env.carla.multi_env import DEFAULT_MULTIENV_CONFIG
 from ray.rllib.evaluation.policy_evaluator import PolicyEvaluator
 from ray.rllib.agents.pg.pg_policy_graph import PGPolicyGraph
 
@@ -8,10 +9,17 @@ from ray.rllib.agents.pg.pg_policy_graph import PGPolicyGraph
 BATCH_COUNT = 2
 EPISODE_HORIZON = 10
 
+# TODO: Add test to make sure DEFAULT_MULTIENV_CONFIG is valid
+configs = DEFAULT_MULTIENV_CONFIG
+configs["env"]["render"] = False
+
 
 def test_rllib_policy_eval():
+    assert (
+        not configs["env"]["render"]), "Tests should be run with render=False"
     evaluator = PolicyEvaluator(
-        env_creator=lambda _: MultiCarlaEnv(),
+        env_creator=lambda _: MultiCarlaEnv(configs),
+        # TODO: Remove the hardcoded spaces
         policy_graph={
             "def_policy": (PGPolicyGraph, Box(0.0, 255.0, shape=(84, 84, 3)),
                            Box(-1.0, 1.0, shape=(2, )), {
