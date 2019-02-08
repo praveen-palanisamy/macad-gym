ac58c9baff6bc1e42ceb72d89e52ac7b29dcf3f7
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2019-02-08 08:35:19 -0500

    Fixed the episode loop

--- env/envs/intersection/urban_2_car_1_ped.py
+++ env/envs/intersection/urban_2_car_1_ped.py
@@ -41,38 +41,36 @@ def get_next_actions(measurements, is_discrete_actions):
 
 for ep in range(2):
     obs = env.reset()
-    for _ in range(2):
-        obs = env.reset()
-        total_vehicle = env.num_vehicle
+    total_vehicle = env.num_vehicle
 
-        total_reward_dict = {}
-        action_dict = {}
+    total_reward_dict = {}
+    action_dict = {}
 
-        env_config = configs["env"]
-        actor_configs = configs["actors"]
-        for actor_id in actor_configs.keys():
-            total_reward_dict[actor_id] = 0
-            if env.discrete_actions:
-                action_dict[actor_id] = 3  # Forward
-            else:
-                action_dict[actor_id] = [1, 0]  # test values
+    env_config = configs["env"]
+    actor_configs = configs["actors"]
+    for actor_id in actor_configs.keys():
+        total_reward_dict[actor_id] = 0
+        if env.discrete_actions:
+            action_dict[actor_id] = 3  # Forward
+        else:
+            action_dict[actor_id] = [1, 0]  # test values
 
-        start = time.time()
-        i = 0
-        done = {"__all__": False}
-        while not done["__all__"]:
-            # while i < 20:  # TEST
-            i += 1
-            obs, reward, done, info = env.step(action_dict)
-            # action_dict = get_next_actions(info, env.discrete_actions)
-            for actor_id in total_reward_dict.keys():
-                total_reward_dict[actor_id] += reward[actor_id]
-            print(":{}\n\t".join(["Step#", "rew", "ep_rew", "done{}"]).format(
-                i, reward, total_reward_dict, done))
+    start = time.time()
+    i = 0
+    done = {"__all__": False}
+    while not done["__all__"]:
+        # while i < 20:  # TEST
+        i += 1
+        obs, reward, done, info = env.step(action_dict)
+        # action_dict = get_next_actions(info, env.discrete_actions)
+        for actor_id in total_reward_dict.keys():
+            total_reward_dict[actor_id] += reward[actor_id]
+        print(":{}\n\t".join(["Step#", "rew", "ep_rew", "done{}"]).format(
+            i, reward, total_reward_dict, done))
 
-            time.sleep(0.1)
+        time.sleep(0.1)
 
-        print("{} fps".format(i / (time.time() - start)))
+    print("{} fps".format(i / (time.time() - start)))
 
 # Clean actors in world
 env.clean_world()
