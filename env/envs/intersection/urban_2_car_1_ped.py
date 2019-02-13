21552a41f171903802a0d0e3c01b507b555a6b11
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2019-02-13 14:21:38 -0500

    Reused get_next_action func from base env impl

--- env/envs/intersection/urban_2_car_1_ped.py
+++ env/envs/intersection/urban_2_car_1_ped.py
@@ -3,41 +3,11 @@ import json
 import time
 
 from env.carla.multi_env import MultiCarlaEnv
+# from env.carla.multi_env import get_next_actions
 
 config_file = open("urban_2_car_1_ped.json")
 configs = json.load(config_file)
 env = MultiCarlaEnv(configs)
-
-
-def get_next_actions(measurements, is_discrete_actions):
-    """Get/Update next action, work with way_point based planner.
-
-    Args:
-        measurements (dict): measurement data.
-        is_discrete_actions (bool): whether use discrete actions
-
-    Returns:
-        dict: action_dict, dict of len-two integer lists.
-    """
-    action_dict = {}
-    for actor_id, meas in measurements.items():
-        m = meas
-        command = m["next_command"]
-        if command == "REACH_GOAL":
-            action_dict[actor_id] = 0
-        elif command == "GO_STRAIGHT":
-            action_dict[actor_id] = 3
-        elif command == "TURN_RIGHT":
-            action_dict[actor_id] = 6
-        elif command == "TURN_LEFT":
-            action_dict[actor_id] = 5
-        elif command == "LANE_FOLLOW":
-            action_dict[actor_id] = 3
-        # Test for discrete actions:
-        if not is_discrete_actions:
-            action_dict[actor_id] = [1, 0]
-    return action_dict
-
 
 for ep in range(2):
     obs = env.reset()
