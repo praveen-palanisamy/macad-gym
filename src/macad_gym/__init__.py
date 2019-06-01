a519bdc9e3041b077a10e1307add5b0c750f2aad
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2019-06-01 15:51:24 -0400

    Simplified MACAD Gym env registrations; -Added an easy-to-maintain Gym namespec-entrypoint mapping

--- src/macad_gym/__init__.py
+++ src/macad_gym/__init__.py
@@ -10,12 +10,12 @@ if not os.path.isdir(LOG_DIR):
 # Init and setup the root logger
 logging.basicConfig(filename=LOG_DIR + '/macad-gym.log', level=logging.DEBUG)
 
-register(
-    id='HomoNcomIndePOIntrxMASS3CTWN3-v0',
-    entry_point="macad_gym.envs:HomoNcomIndePOIntrxMASS3CTWN3",
-)
+AVAILABLE_ENVS = {
+    'HomoNcomIndePOIntrxMASS3CTWN3-v0':
+    "macad_gym.envs:HomoNcomIndePOIntrxMASS3CTWN3",
+    'HeteNcomIndePOIntrxMATLS1B2C1PTWN3-v0':
+    "macad_gym.envs:HeteNcomIndePOIntrxMATLS1B2C1PTWN3",
+}
 
-register(
-    id='HeteNcomIndePOIntrxMATLS1B2C1PTWN3-v0',
-    entry_point="macad_gym.envs:HeteNcomIndePOIntrxMATLS1B2C1PTWN3",
-)
+for id, entry_point in AVAILABLE_ENVS.items():
+    register(id=id, entry_point=entry_point)
