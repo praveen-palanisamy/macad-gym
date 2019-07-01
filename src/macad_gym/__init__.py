15bb7111e0c65265256fc0195745b189724952a6
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2019-06-30 23:12:45 -0700

    Added a module method to list available envs with a short-description

--- src/macad_gym/__init__.py
+++ src/macad_gym/__init__.py
@@ -10,12 +10,34 @@ if not os.path.isdir(LOG_DIR):
 # Init and setup the root logger
 logging.basicConfig(filename=LOG_DIR + '/macad-gym.log', level=logging.DEBUG)
 
-AVAILABLE_ENVS = {
-    'HomoNcomIndePOIntrxMASS3CTWN3-v0':
-    "macad_gym.envs:HomoNcomIndePOIntrxMASS3CTWN3",
-    'HeteNcomIndePOIntrxMATLS1B2C1PTWN3-v0':
-    "macad_gym.envs:HeteNcomIndePOIntrxMATLS1B2C1PTWN3",
+_AVAILABLE_ENVS = {
+    'HomoNcomIndePOIntrxMASS3CTWN3-v0': {
+        "entry_point":
+        "macad_gym.envs:HomoNcomIndePOIntrxMASS3CTWN3",
+        "description":
+        "Homogenous, Non-communicating, Independed, Partially-"
+        "Observable Intersection Multi-Agent scenario with "
+        "Stop-Sign, 3 Cars in Town3, version 0"
+    },
+    'HeteNcomIndePOIntrxMATLS1B2C1PTWN3-v0': {
+        "entry_point":
+        "macad_gym.envs:HeteNcomIndePOIntrxMATLS1B2C1PTWN3",
+        "description":
+        "Heterogeneous, Non-communicating, Independent,"
+        "Partially-Observable Intersection Multi-Agent"
+        " scenario with Traffic-Light Signal, 1-Bike, 2-Car,"
+        "1-Pedestrian in Town3, version 0"
+    }
 }
 
-for id, entry_point in AVAILABLE_ENVS.items():
-    register(id=id, entry_point=entry_point)
+for id, val in _AVAILABLE_ENVS.items():
+    register(id=id, entry_point=val.get("entry_point"))
+
+
+def list_available_envs():
+    print("Environment-ID: Short-description")
+    import pprint
+    available_envs = {}
+    for env_id, val in _AVAILABLE_ENVS.items():
+        available_envs[env_id] = val.get("description")
+    pprint.pprint(available_envs)
