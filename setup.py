d758393f790421c7a0335b9a42d04479a3cc20ae
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2019-10-12 19:31:11 -0700

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import os
+
+# Prepare long description using existing docs
+long_description = ""
+this_dir = os.path.abspath(os.path.dirname(__file__))
+doc_files = ["README.md"]
+for doc in doc_files:
+    with open(os.path.join(this_dir, doc), 'r') as f:
+        long_description = "\n".join([long_description, f.read()])
+# Replace relative path to images with Github URI
+github_uri_prefix = "https://raw.githubusercontent.com/praveen-palanisamy" \
+                    "/macad-gym/master/"
+rel_img_path = "docs/images/"
+long_description = long_description.replace(
+    "(" + rel_img_path, "(" + github_uri_prefix + rel_img_path)
 
 setup(
     name="macad-gym",
-    description='Learning environments for Multi-Agent Connected Autonomous'
-    'Driving (MACAD) with OpenAI Gym compatible interfaces, built using CARLA',
     version='0.1.0',
+    description='Learning environments for Multi-Agent Connected Autonomous'
+    'Driving (MACAD) with OpenAI Gym compatible interfaces',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/praveen-palanisamy/macad-gym',
     author='Praveen Palanisamy',
     author_email='praveen.palanisamy@outlook.com',
