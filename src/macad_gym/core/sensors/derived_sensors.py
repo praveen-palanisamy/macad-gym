e08ed1bd7fa17f032a9bc72f0c0d047e1ea55a22
Morphlng <50166287+Morphlng@users.noreply.github.com>
2022-10-02 16:57:21 +0800

    correct sensor blueprint naming

--- src/macad_gym/core/sensors/derived_sensors.py
+++ src/macad_gym/core/sensors/derived_sensors.py
@@ -16,7 +16,7 @@ class LaneInvasionSensor(object):
         self.offlane = 0  # count of off lane
         self.offroad = 0  # count of off road
         world = self._parent.get_world()
-        bp = world.get_blueprint_library().find('sensor.other.lane_detector')
+        bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
         self.sensor = world.spawn_actor(
             bp, carla.Transform(), attach_to=self._parent)
         # We need to pass the lambda a weak reference to self to avoid circular
